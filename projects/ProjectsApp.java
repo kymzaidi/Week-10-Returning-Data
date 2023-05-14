db79136db294f926c1485e9494b9c6cda5ecfac8
kymzaidi <a.kymbat1@gmail.com>
2023-05-13 22:04:18 -0500

    first commit

--- projects/ProjectsApp.java
+++ projects/ProjectsApp.java
@@ -11,10 +11,13 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	
 	// @formatter:off
 	private List<String> operations = List.of(
-			"1) Add a project"
+			"1) Add a project",
+			"2) List projects",
+			"3) Select a project"
 );
 	// @formatter:on
 	
@@ -38,6 +41,14 @@ public class ProjectsApp {
 				createProject();
 				break;
 				
+			case 2:
+				listProjects();
+				break;
+				
+			case 3:
+				selectProject();
+				break;
+				
 			default:
 				System.out.println("\n" + selection + " is not a valid selection. Try again.");
 				break;
@@ -45,8 +56,26 @@ public class ProjectsApp {
 		}
 		catch(Exception e) {
 			System.out.println("\nError:" + e + " Try again.");
+			
+			e.printStackTrace();
 		}
 	}
+		
+	}
+
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput ("Enter a project ID to select a project");
+		curProject = null;
+		curProject = projectService.fetchProjectById(projectId);
+	}
+
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		System.out.println("\nProjects:");
+		projects.forEach(project -> System.out
+				.println(" " + project.getProjectId()
+				+ ". " + project.getProjectName() ));
 		
 	}
 
@@ -56,7 +85,7 @@ public class ProjectsApp {
 	}
 
 	private void createProject() {
-		String projectName = getStringInput ("Enter the estimated hours");
+		String projectName = getStringInput ("Enter the project name");
 		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
 		BigDecimal actualHours = getDecimalInput ("Enter the actual hours");
 		Integer difficulty = getIntInput ("Enter the project difficulty (1-5)");
@@ -125,6 +154,13 @@ public class ProjectsApp {
 		
 		for(String line : operations) {
 			System.out.println(" " + line);
+		}
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		}
+		else {
+			System.out.println("\nYou are working with project: " + curProject);
 		}
 				
 	}
